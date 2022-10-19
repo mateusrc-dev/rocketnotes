7478e4e69b954f52443eda8855c8b3784e3a607f
Mateus Carvalho <mateus_raimundo95@outlook.com>
2022-10-19 15:34:34 -0300

    updated baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 //vamos deixar aqui as configurações do axios
 import axios from "axios" //importando o axios
 export const api = axios.create({//vamos usar o método dentro de axios chamado create - podemos passar um objeto com configurações
-  baseURL: "http://localhost:3333", //podemos incluir nessa propriedade baseUrl parte do endereço da api que se repete em todas as requisições para não precisar ficar repetindo ela nas requisições
+  baseURL: "https://rocketnotesss-api.herokuapp.com", //podemos incluir nessa propriedade baseUrl parte do endereço da api que se repete em todas as requisições para não precisar ficar repetindo ela nas requisições
 
 }) 
 
